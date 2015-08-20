48c627d7dbf474e8f8dcf3ca6fd0c29c23dc69a0
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2015-08-20 10:05:29 -0500

    Add some task in TODO

--- main.py
+++ main.py
@@ -21,6 +21,14 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 
+# TODO
+# * facilitar la descarga de aqua a traves de un nuevo argumento
+# * la descarga de aqua y terra queden en carpetas distintas terra/p0... aqua/p0....
+# * arreglas la nomenclatura de la carpeta principal
+# * verificar la integridad de datos, problemas de descarga (sin internet, no url)
+# * crear un ejecutable para correr todo_ el proceso segundo plano (nohub)
+
+
 ########################################## arguments ##########################################
 
 
