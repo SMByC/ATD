72ad77e299e57574ad0883656b6c61d572ee57ac
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2016-05-01 22:34:52 -0500

    implement version of the project

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 from setuptools import setup, find_packages
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
     README = f.read()
+with open(os.path.join(HERE, 'atd', '__init__.py'), encoding='utf-8') as fp:
+    VERSION = re.search("__version__ = '([^']+)'", fp.read()).group(1)
 
 setup(
     name='ATD',
-    version='0.1',
+    version=VERSION,
     description='Automated Process of Early Alert Deforestation',
     long_description=README,
     author='Xavier Corredor Llano, SMBYC-IDEAM',
