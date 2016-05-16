c5f0e53f558e24e8500a540ecf4c226e2a8bb76b
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2016-05-16 18:33:58 -0500

    fixed reprojections using two step, first to WGS LatLon (EPSG:4326) and after to WGS 84 / UTM zone 18N (EPSG:32618), and adjusted pixel to 500 and 250

--- atd/process/p2_reprojection.py
+++ atd/process/p2_reprojection.py
@@ -42,32 +42,57 @@ def run(config_run):
         if len(files) != 0:
             files = [x for x in files if x.endswith(('.tif', '.TIF'))]
             for file in files:
-                in_file = os.path.join(root, file)
-                out_file = os.path.join(dir_process, file)
-
                 msg = 'Reprojection file {0} with Gdal: '.format(file)
                 config_run.process_logfile.write(msg)
                 config_run.process_logfile.flush()
                 print(msg)
 
+                ##############
+                # first reprojection to The WGS LatLon (EPSG:4326)
+                in_file = os.path.join(root, file)
+                file_tmp = "tmp_"+file
+                out_file_tmp = os.path.join(dir_process, file_tmp)
+
                 # Reprojection with Gdal
-                # gdalwarp h10v08_MOD09A1_band07.tif h10v08_MOD09A1_reproj.tif -r near -t_srs EPSG:32618
-                return_code = call(["gdalwarp", in_file, out_file, "-r", "near", "-t_srs", 'EPSG:32618'])
+                return_code = call(["gdalwarp", in_file, out_file_tmp, "-r", "near", "-t_srs", "EPSG:4326"])
 
                 if return_code == 0:  # successfully
-                    msg = 'was reprojected successfully'
-                else:
-                    msg = '\nError: Problem with reprojecting in Gdal\n'
-
-                if return_code == 0:  # successfully
-                    msg = 'was reprojected successfully'
+                    msg = 'was reprojected to EPSG:4326 successfully'
                     config_run.process_logfile.write(msg + '\n')
                     print(msg)
                 else:
-                    msg = '\nError: Problems reprojecting in Gdal\n'
+                    msg = '\nError: Problem with reprojecting in Gdal\n'
                     config_run.process_logfile.write(msg + '\n')
                     print(msg)
                     break
+
+                ##############
+                # second reprojection to WGS 84 / UTM zone 18N (EPSG:32618)
+                out_file = os.path.join(dir_process, file)
+                mode = file.split('_')[1]
+
+                # process the mode MXD09A1: adjust pixel size
+                if mode in ['MOD09A1', 'MYD09A1']:
+                    pixel_size = ["500","500"]
+                # process the mode MXD09Q1: adjust pixel size
+                if mode in ['MOD09Q1', 'MYD09Q1']:
+                    pixel_size = ["250", "250"]
+
+                # Reprojection with Gdal
+                return_code = call(["gdalwarp", out_file_tmp, out_file,
+                                    "-r", "near", "-t_srs", "EPSG:32618", "-tr"] + pixel_size)
+
+                if return_code == 0:  # successfully
+                    msg = 'was reprojected to EPSG:32618 successfully'
+                    config_run.process_logfile.write(msg + '\n')
+                    print(msg)
+                else:
+                    msg = '\nError: Problem with reprojecting in Gdal\n'
+                    config_run.process_logfile.write(msg + '\n')
+                    print(msg)
+                    break
+
+                os.remove(out_file_tmp)
 
     # finishing the process
     msg = '\nThe process {0} completed {1}- ({2})'.format(config_run.process_name,
