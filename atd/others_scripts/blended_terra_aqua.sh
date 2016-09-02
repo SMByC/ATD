5e3977108040829d24da3163cd76d84a7ef78379
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2016-09-02 17:48:30 -0500

    Added script for generate the blended layerstack of terra and aqua

---
+++ atd/others_scripts/blended_terra_aqua.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+#  (c) Copyright SMBYC - IDEAM 2014-2016
+#  Authors: Xavier Corredor Llano
+#  Email: xcorredorl at ideam.gov.co
+
+bands1=79
+bands2=88
+
+for file1 in p3_mosaic1/*.tif
+do
+    echo $file1
+    mkdir blended_tmp
+
+    for band in $(seq 1 $bands1); do
+        gdal_translate -b ${band} $file1 blended_tmp/${band}.tif
+    done
+
+    file2=$(echo $file1 | sed 's/p3_mosaic1/p3_mosaic2/g')
+    for band in $(seq 1 $bands2); do
+        gdal_translate -b ${band} $file2 blended_tmp/$((bands1+band)).tif
+    done
+
+    file_out=$(echo $file1 | sed 's/p3_mosaic1/p3_mosaic/g')
+    gdal_merge.py -a_nodata '-28672' -co COMPRESS=LZW -co PREDICTOR=2 \
+      -co TILED=YES -co BIGTIFF=YES -o ${file_out} -separate blended_tmp/*.tif
+
+    rm -rf blended_tmp
+done
+
