244bc8f4900fd50926c9b923a0654c17797424d4
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-08-09 16:25:29 -0500

    Use UInt32 for std

--- atd/process/p4_stats.py
+++ atd/process/p4_stats.py
@@ -510,10 +510,10 @@ def multiprocess_statistic(stat, in_file, layerstack_chunks, out_file, prev_laye
         output_array = np.memmap.dot(output_array, 100)
         output_type = gdal.GDT_Int16
     # convert std statistic to integer values and multiply x100
-    # UInt16 range is 0 to 65535
+    # UInt32
     if stat in ['std']:
         output_array = np.memmap.dot(output_array, 100)
-        output_type = gdal.GDT_UInt16
+        output_type = gdal.GDT_UInt32
     # define the default output type format.
     # UInt16 range is 0 to 65535
     if stat in ['median', 'mean', 'valid_data']:
